d058e2f6d0dd40ccc293893c012c678333e7ae9e
bmpc <bcipriano@gmail.com>
2016-11-26 10:54:53 +0000

    Added main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  fmt.Println("Welcome to the real world");
+
+  var x int
+  var y int
+
+  x = 10
+  y = x + 1
+
+  fmt.Println("ola", " ", x, " ", y, " ", 999);
+
+  fmt.Println("div 3/4: ", 3 / 4);
+
+  fmt.Println("div 3.0/4: ", 3.0 / 4);
+
+}
